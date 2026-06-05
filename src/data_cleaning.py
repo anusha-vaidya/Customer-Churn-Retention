a89b4f460afd4c4069e9228ca9d7947a6075b3da
Anusha Vaidya <vaidyaanu96@gmail.com>
2026-06-05 15:33:02 -0400

    Update data_cleaning.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,1 +1,9 @@
+import pandas as pd
+
+def clean_data(filepath):
+    df = pd.read_csv(filepath)
+    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+    df['TotalCharges'].fillna(df['TotalCharges'].median(), inplace=True)
+    df.drop(columns=['customerID'], inplace=True)
+    return df
 
